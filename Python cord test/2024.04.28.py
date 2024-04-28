d779b3a5e80723f6c3caaa606791b5f66fb3a365
Youndong99 <166194297+Youndong99@users.noreply.github.com>
2024-04-28 20:28:19 +0900

    fix14

---
+++ Python cord test/2024.04.28.py
@@ -0,0 +1,22 @@
+
+
+#continue
+
+for i in range(1,11):
+    if i==5:
+        continue
+    print(i)
+
+
+#break
+for i in range(1,11):
+    if i==5:
+        break
+    print(i)
+
+
+#pass
+for i in range(1,11):
+    if i==5:
+        pass
+    print(i)
